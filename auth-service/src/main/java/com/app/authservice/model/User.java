0adc0d05aafb8a11d536f0729f83e7968b8f5bef
Uday Teja <dev@udayteja.com>
2026-02-15 16:53:23 +0530

    add schema

---
+++ auth-service/src/main/java/com/app/authservice/model/User.java
@@ -0,0 +1,46 @@
+package com.app.authservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_user_email", columnList = "email")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 40)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Column(
+            name = "created_at",
+            nullable = false,
+            updatable = false,
+            insertable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+    )
+    private LocalDateTime createdAt;
+
+}
