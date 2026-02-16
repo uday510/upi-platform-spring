9ad1723c021bca7657acdf28fb654be7adfa278c
Uday Teja <dev@udayteja.com>
2026-02-16 14:23:24 +0530

    Fix gitignore

---
+++ notification-service/src/test/java/com/app/notificationservicespring/NotificationServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.notificationservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ upi-service/src/main/java/com/app/upi/UpiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.upi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpiServiceApplication.class, args);
+    }
+
+}
